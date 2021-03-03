d9f44fec47cda09a9adbebbfb70e2930cacfb5d2
copoha <coralinepozzi@hotmail.fr>
2021-03-03 15:48:55 +0100

    code controller

---
+++ controller/src/main/java/helloworldmvc/controller/Controller.java
@@ -0,0 +1,22 @@
+package helloworldmvc.controller;
+
+import helloworldmvc.model.IModel;
+import helloworldmvc.view.IView;
+
+import java.io.FileNotFoundException;
+
+public class Controller {
+    private IModel model;
+    private IView view;
+
+    Controller(IModel model, IView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void run() throws FileNotFoundException {
+        String str = this.model.getMessage();
+        this.view.display(str);
+    }
+
+}
